a361610efaf19f515858a68c57cf37b284b9b301
elisi77 <121964742+elisi77@users.noreply.github.com>
2023-03-29 14:21:08 -0300

    Add files via upload

---
+++ PORTAFOLIO M3/assets/js/productos.js
@@ -0,0 +1,67 @@
+let productos = [
+    {
+
+        sku: "001" , 
+        nombre: "Acqua di gio - Giorgio Armani 100ml" ,
+        descripcion: "Perfume hombre EDT" , 
+        precio: 110000, 
+        descuento: 9990,
+        imagen: "./assets/img/acquadigio.jpg"
+
+    }, 
+
+    {
+        
+        sku: "002" , 
+        nombre: "Poue Elle Sparklink - Lacoste 50ml" ,
+        descripcion: "Perfume mujer EDT" , 
+        precio: 60000, 
+        descuento: 17000,
+        imagen: "./assets/img/lacoste.jpg"
+
+    }, 
+
+    {
+        
+        sku: "003" , 
+        nombre: "La vie belle - Lancome 50ml" ,
+        descripcion: "Perfume mujer EDP" , 
+        precio: 78000, 
+        descuento: 30000,
+        imagen: "./assets/img/laviebelle.jpg"
+
+    }, 
+
+    {
+        
+        sku: "004" , 
+        nombre: "Miracle - Lancome 30ml" ,
+        descripcion: "Perfume mujer EDP" , 
+        precio: 59990,
+        descuento: 30000, 
+        imagen: "./assets/img/miracle.jpg"
+
+    }, 
+
+    {
+        
+        sku: "005" , 
+        nombre: "My way - Giorgio Armani 30ml" ,
+        descripcion: "Perfume mujer EDP" , 
+        precio: 56990, 
+        descuento: 9990,
+        imagen: "./assets/img/myway.jpg"
+
+    }, 
+
+    {
+        
+        sku: "006" , 
+        nombre: "Sisterland Pink Raspberry - Benetton 80ml" ,
+        descripcion: "Perfume mujer EDT" , 
+        precio: 24000,
+        descuento: 3000,
+        imagen: "./assets/img/unitedcolors.jpg"
+
+    },  
+  ];
